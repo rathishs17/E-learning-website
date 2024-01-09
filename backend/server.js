926ca684c6dc65c291b78e23c9861a47d4315c3e
rathishs17 <rathishs777@gmail.com>
2024-01-09 21:12:01 +0530

    add and del bckend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,34 @@
-const express =require('express');
-const app=express();
-const port=8000;
-require('./database/conn')
-const product_data=require('./database/product_model');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 1000;
+const bodyParser = require('body-parser');
+require('./database/conn');
+app.use(bodyParser());
+app.use(cors())
+const product_data = require('./database/product_model')
 
 app.get('/',async(req,res)=>{
-   const response= await product_data.find({});
-   res.json(response)
+        const response = await product_data.find({});
+        res.json(response)
+})
+app.post('/newProduct',(req,res)=>{
+        const newProduct = new product_data(req.body);
+        newProduct.save()
+        .then(()=>res.json({message:"Added product successfully"}))
+        .catch(err=>res.json({message:err}))
+        
+})
+// get, post, delete, put
+app.delete('/deleteProduct/:name',(req,res)=>{
+        const {name} = req.params;
+        console.log(req.params);
+        product_data.deleteOne({name:name})
+        .then(()=>res.json({message:"Delete product successfully"}))
+        .catch((err)=>res.json({message:err}))
+
 })
 
-
-
 app.listen(port,()=>{
-    console.log(`connected`);
+        console.log(`Connected to port ${port} 🚀`);
 })
