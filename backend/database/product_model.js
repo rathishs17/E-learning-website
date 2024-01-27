f16278975cf7b57408d160fd937dc25283644f80
rathishs17 <rathishs777@gmail.com>
2024-01-27 20:35:05 +0530

    alignment done

--- backend/database/product_model.js
+++ backend/database/product_model.js
@@ -3,7 +3,8 @@ const mongoose =require('mongoose');
 const product_schema=new mongoose.Schema({
     name:{type:String, required:true},
     img:{type:String, required:true},
-    cost:{type:Number, required:true}
+    cost:{type:Number, required:true},
+
 })
 
 module.exports=new mongoose.model('Course',product_schema)
